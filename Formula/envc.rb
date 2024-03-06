a8632f844316d4292dd453fa3149cbae21ddcc87
Michiel Nijenhuis <michiel@totaldesign.com>
2024-03-06 21:03:57 +0100

    feat: add update homebrew formula workflow

--- Formula/envc.rb
+++ Formula/envc.rb
@@ -1,8 +1,8 @@
 class Envc < Formula
     desc "A program that compares two .env files and shows their values and differences."
     homepage "https://github.com/michielnijenhuis/env-check"
-    url "https://example.com/my-cli/archive/v1.0.0.tar.gz"
-    sha256 "..."
+    url "url"
+    sha256 "sha256"
     license "MIT"
     
     depends_on "make"
