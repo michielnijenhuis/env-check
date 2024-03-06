1ed8ebb6a34e6a33602f62168784de72c44974e6
Michiel Nijenhuis <michiel@totaldesign.com>
2024-03-06 21:36:24 +0100

    release: 1.2.3

--- Formula/envc.rb
+++ Formula/envc.rb
@@ -1,8 +1,8 @@
 class Envc < Formula
     desc "A program that compares two .env files and shows their values and differences."
     homepage "https://github.com/michielnijenhuis/env-check"
-    url "url"
-    sha256 "sha256"
+    url "https://github.com/michielnijenhuis/env-check/archive/refs/tags/v1.2.3.tar.gz"
+    sha256 "6531f47d2703b03e3b35802b4b45657e3e80a646133494ac45dcad368cadf13e"
     license "MIT"
     
     depends_on "make"
