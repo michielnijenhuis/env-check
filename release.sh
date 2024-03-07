6902bf4dbc544bbb968539503c96624c8d3b4d75
Michiel Nijenhuis <michiel@totaldesign.com>
2024-03-07 09:02:33 +0100

    feat: remove build.sh, use Makefile instead, add gh workflow to update formula

--- release.sh
+++ release.sh
@@ -25,8 +25,6 @@ LIBS=(
     "usage"
 )
 
-VERSION=$(git describe --tags --abbrev=0 | sed 's/^v//')
-
 for LIB in "${LIBS[@]}"; do
     FILE="/usr/local/include/$LIB.h"
     
@@ -43,25 +41,4 @@ for LIB in "${LIBS[@]}"; do
     fi
 done
 
-BUILD_SCRIPT="./build.sh"
-touch "$BUILD_SCRIPT"
-printf "#!/bin/bash\n\n" > $BUILD_SCRIPT
-{
-    printf "CMD=\"\$*\"\n\n";
-    printf "if [ -z \"\$CMD\" ]; then\n";
-    printf "\tCMD=\"clang -Wall -O2\"\n";
-    printf "fi\n\n"
-    printf "set -x\n\n";
-} >> $BUILD_SCRIPT
-
-printf "\$CMD -DENVC_VERSION=%s%s%s -Idist -o bin/envc dist/main.c" '\"' "$VERSION" '\"' >> "$BUILD_SCRIPT"
-for LIB in "${LIBS[@]}"; do
-    if [ -f "dist/$LIB.c" ]; then
-        printf " dist/%s.c" "$LIB" >> "$BUILD_SCRIPT"
-    fi
-done
-printf "\n"
-
 cp "src/main.c" "dist/main.c"
-
-compile -r -DENVC_VERSION=\\\""$VERSION"\\\" -o bin/main src/main
